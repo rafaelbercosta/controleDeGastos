cf44ba8b78570642d33dd95755211016410233ea
rafael <rafaelbercosta@gmail.com>
2025-04-22 00:49:37 -0300

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={ store }>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+
   </Provider>,
   document.getElementById('root'),
 );
